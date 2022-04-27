b88d1197396cb3ec98399ca9e8df94f67d49b3d1
Ole Jørgen Abusdal <ojab@pc-guest3.se.informatik.tu-darmstadt.de>
2022-04-27 13:44:13 +0200

    listings for first example

---
+++ paper/listings/ReplaceArrayWithObject/after.java
@@ -0,0 +1,4 @@
+Coordinate coordinate = new Coordinate();
+
+coordinate.setLatitude("36.452999");
+coordinate.setLongitude("28.226376");
---
+++ paper/listings/ReplaceArrayWithObject/before.java
@@ -0,0 +1,4 @@
+String[] coordinate = new String[2];
+
+coordinate[0] = "36.452999";
+coordinate[1] = "28.226376";
